5c5a92918da54ae38d3d1076bac1954bca3ad553
Tim Otten <totten@civicrm.org>
2019-01-24 19:22:45 -0800

    default.nix - Improve readability

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
-## By defualt, pin to a revision nixpkgs 18.09
-{pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/299814b385d2c1553f60ada8216d3b0af3d8d3c6.tar.gz) {
+## locolamp: Download a collection of binaries for use in "LAMP"-style development
+## (Apache, MySQL, PHP, NodeJS, etc). Binaries are downloaded from nixpkgs.
+
+## By default, we'll track nixpkgs v18.09. To upgrade, change the URL to a different branch/tag/commit.
+{
+  pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz) {
     inherit system;
   },
   system ? builtins.currentSystem,
-  }:
+}:
 
 let
   stdenv = pkgs.stdenv;
@@ -13,13 +17,16 @@ let
     in f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
 
   extraPkgs = {
-    loco = callPackage (fetchTarball https://github.com/totten/loco/archive/master.tar.gz) {};
+    ## By default, we've pinned to a specific version of loco. We could grab a developmental version from Github or a local clone.
+    loco = callPackage (fetchTarball https://github.com/totten/loco/archive/v0.1.0.tar.gz) {};
+    # loco = callPackage (fetchTarball https://github.com/totten/loco/archive/master.tar.gz) {};
     # loco = callPackage /home/myuser/src/loco {};
   };
 
 in stdenv.mkDerivation rec {
     name = "locolamp";
 
+    ## This is the main list of packages that we want.
     buildInputs = [
       /* Major services */
       pkgs.php72
@@ -42,9 +49,9 @@ in stdenv.mkDerivation rec {
       pkgs.unzip
       pkgs.which
       pkgs.zip
-
     ];
 
+  ## When starting `nix-shell`, load all the project-wide config variables from `loco.yml`.
   shellHook = ''
     eval $(loco env --export)
   '';
